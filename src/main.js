4eefa0f812fd4c765a522967bd9ad1f1da5c214e
hrojah <hrojah@MacBook-Pro-hrojah.local>
2021-04-08 22:09:35 +0500

    Добавит показ оставшихся фильмов и звание пользователя

--- src/main.js
+++ src/main.js
@@ -9,12 +9,15 @@ import {createTopRatedTemplate} from './view/top-rated';
 import {createMostCommentedTemplate} from './view/most-commented';
 import {createAmountFilmsTemplate} from './view/amount-films';
 import {generateFilm} from './mock/film';
+import {generateFilter} from "./mock/filter";
 
-export const FILM_COUNT = 20;
+const FILM_COUNT = 20;
+const FILM_COUNT_PER_STEP = 5;
 
 const films = new Array(FILM_COUNT).fill().map(generateFilm);
 const topRatedFilms = films.slice().sort((a, b) => b.rating - a.rating);
 const mostCommentsFilms = films.slice().sort((a,b) => b.comments.length - a.comments.length);
+const filters = generateFilter(films);
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
@@ -26,8 +29,8 @@ const siteMainElement = document.querySelector('.main');
 const footerStatisticsElement = document.querySelector('.footer__statistics');
 
 
-render (siteHeaderElement, createRankUserTemplate(), 'beforeend');
-render (siteMainElement, createMenuNavTemplate(), 'beforeend');
+render (siteHeaderElement, createRankUserTemplate(filters), 'beforeend');
+render (siteMainElement, createMenuNavTemplate(filters), 'beforeend');
 render (siteMainElement, createSortTemplate(), 'beforeend');
 render (siteMainElement, createFilmListTemplate(), 'beforeend');
 
@@ -36,10 +39,30 @@ const filmsListElement = document.querySelector('.films-list');
 const cardContainerElement = document.querySelector('.films-list__container');
 // render (siteBodyElement, createPopupTemplate(films[0]), 'beforeend');
 
-for (let i = 0; i < FILM_COUNT; i++) {
+for (let i = 0; i < Math.min(films.length, FILM_COUNT_PER_STEP); i++) {
   render(cardContainerElement, createFilmCardTemplate(films[i]), 'beforeend');
 }
-render (filmsListElement, createShowMoreButtonTemplate(), 'beforeend');
+
+if (films.length > FILM_COUNT_PER_STEP) {
+  let renderedFilmCount = FILM_COUNT_PER_STEP;
+  render(filmsBoardElement, createShowMoreButtonTemplate(), 'beforeend');
+
+  const loadMoreButton = filmsBoardElement.querySelector('.films-list__show-more');
+
+  loadMoreButton.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    films
+      .slice(renderedFilmCount, renderedFilmCount + FILM_COUNT_PER_STEP)
+      .forEach((film) => render(cardContainerElement, createFilmCardTemplate(film), 'beforeend'));
+
+    renderedFilmCount += FILM_COUNT_PER_STEP;
+
+    if (renderedFilmCount >= films.length) {
+      loadMoreButton.remove();
+    }
+  });
+}
+// render (filmsListElement, createShowMoreButtonTemplate(), 'beforeend');
 render (filmsBoardElement, createTopRatedTemplate(), 'beforeend');
 
 const topRatedFilmsContainer = document.querySelector('.top-rated');
